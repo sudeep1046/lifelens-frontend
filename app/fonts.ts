8ad5c1840749283140cb799d6890c94898176634
sudeep1046 <your_github_email@example.com>
2026-04-12 00:25:44 +0530

    lifelens frontend

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+import { Orbitron, Inter } from "next/font/google";
+
+export const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
